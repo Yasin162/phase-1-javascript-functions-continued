7dde7b2ca184207773943d3b06b2d86266fe417e
Yasin <yasinabdussaburr@gmail.com>
2021-06-16 20:18:00 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code here
+function saturdayFun(defualt='roller-skate') {
+    return `This Saturday, I want to ${defualt}!`
+}
+function mondayWork(defualt='go to the office') {
+    return `This Monday, I will ${defualt}.`
+}
+function wrapAdjective(d='*') {
+    return function inner(defualt= 'special'){
+        return `You are ${d}${defualt}${d}!`
+    }
+    
+}
